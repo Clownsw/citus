28bc291ea6fdbc67127af89c473721a606a9f8b9
Nils Dijk <nils@citusdata.com>
2022-09-01 17:27:05 +0200

    fix test

--- src/test/regress/sql/rebalance_job.sql
+++ src/test/regress/sql/rebalance_job.sql
@@ -9,7 +9,7 @@ CREATE TABLE results (a int);
 -- simple job that inserts 1 into results to show that query runs
 SELECT a FROM results WHERE a = 1; -- verify result is not in there
 INSERT INTO pg_dist_background_jobs (job_type) VALUES ('test') RETURNING job_id \gset
-INSERT INTO pg_dist_background_tasks (job_id, command) VALUES (:job_id, $job$ INSERT INTO rebalance_job.results VALUES ( 1 ); $job$) RETURNING task_id \gset
+INSERT INTO pg_dist_background_tasks (job_id, owner, command) VALUES (:job_id, 'postgres', $job$ INSERT INTO rebalance_job.results VALUES ( 1 ); $job$) RETURNING task_id \gset
 -- TODO: Actually make this work
 SELECT citus_jobs_wait(:job_id); -- wait for the job to be finished
 SELECT a FROM results WHERE a = 1; -- verify result is there
